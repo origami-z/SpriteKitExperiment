21f15a93aea82ea452852d077827534e5416ea70
Zhihao Cui <davidcui2@me.com>
2022-01-01 21:15:55 +0000

    Add high score, make playable on iPhone

--- SpriteKitExperiment/GameScene.swift
+++ SpriteKitExperiment/GameScene.swift
@@ -11,10 +11,16 @@ class Ball: SKSpriteNode { }
 
 class GameScene: SKScene {
     
+    let deviceIdiom = UIScreen.main.traitCollection.userInterfaceIdiom
+    
     var balls = ["ballBlue", "ballGreen", "ballPurple", "ballRed", "ballYellow"]
     var motionManager: CMMotionManager?
     
+    let labelFontSize = UIFont.systemFontSize * 2
+    let highestScoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
     let scoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    let restartLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    let ballsContainerBottomMargin = UIFont.systemFontSize * 4
     var matchedBalls = Set<Ball>()
     
     var score = 0 {
@@ -25,26 +31,18 @@ class GameScene: SKScene {
             scoreLabel.text = "SCORE: \(formattedScore)"
         }
     }
+    var highScore = 0 {
+        didSet {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            let formattedScore = formatter.string(from: highScore as NSNumber) ?? "0"
+            highestScoreLabel.text = "Highest: \(formattedScore)"
+        }
+    }
     
-    override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "checkerboard")
-        background.position = CGPoint(x: frame.midX, y: frame.midY)
-        background.alpha = 0.2
-        background.zPosition = -1
-        addChild(background)
-        
-        scoreLabel.fontSize = 72
-        scoreLabel.position = CGPoint(x: 20, y: 20)
-        scoreLabel.text = "SCORE: 0"
-        scoreLabel.zPosition = 100
-        scoreLabel.horizontalAlignmentMode = .left
-        addChild(scoreLabel)
-        
-        let ball = SKSpriteNode(imageNamed: "ballBlue")
-        let ballRadius = ball.frame.width / 2.0
-        
-        for i in stride(from: ballRadius, to: view.bounds.width - ballRadius, by: ball.frame.width) {
-            for j in stride(from: 100, to: view.bounds.height - ballRadius, by: ball.frame.height) {
+    fileprivate func initializeBalls(_ ballRadius: CGFloat, _ view: SKView, _ ball: SKSpriteNode) {
+        for i in stride(from: ballRadius * 3, to: view.bounds.width - ballRadius * 3, by: ball.frame.width) {
+            for j in stride(from: ballsContainerBottomMargin, to: view.bounds.height - ballRadius, by: ball.frame.height) {
                 let ballType = balls.randomElement()!
                 let ball = Ball(imageNamed: ballType)
                 ball.position = CGPoint(x: i , y: j)
@@ -58,6 +56,43 @@ class GameScene: SKScene {
                 addChild(ball)
             }
         }
+    }
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "checkerboard")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.alpha = 0.2
+        background.zPosition = -1
+        addChild(background)
+        
+        highestScoreLabel.fontSize = labelFontSize
+        highestScoreLabel.position = CGPoint(x: 20, y: 20)
+        highestScoreLabel.text = "Highest: 0"
+        highestScoreLabel.zPosition = 100
+        highestScoreLabel.horizontalAlignmentMode = .left
+        addChild(highestScoreLabel)
+        
+        highScore = UserDefaults.standard.integer(forKey: "high_score")
+        
+        scoreLabel.fontSize = labelFontSize
+        scoreLabel.position = CGPoint(x: view.bounds.midX, y: 20)
+        scoreLabel.text = "SCORE: 0"
+        scoreLabel.zPosition = 100
+        scoreLabel.horizontalAlignmentMode = .center
+        addChild(scoreLabel)
+        
+        restartLabel.name = "RestartButton"
+        restartLabel.fontSize = labelFontSize
+        restartLabel.position = CGPoint(x: view.bounds.width - 20, y: 20)
+        restartLabel.text = "RESTART"
+        restartLabel.zPosition = 100
+        restartLabel.horizontalAlignmentMode = .right
+        addChild(restartLabel)
+        
+        let ball = SKSpriteNode(imageNamed: "ballBlue")
+        let ballRadius = ball.frame.width / 2.0
+        
+        initializeBalls(ballRadius, view, ball)
         
         let uniforms: [SKUniform] = [
             SKUniform(name: "u_speed", float: 1),
@@ -71,7 +106,7 @@ class GameScene: SKScene {
         
         background.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 10)))
         
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)))
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: ballsContainerBottomMargin, left: ballRadius * 2, bottom: 0, right: ballRadius * 2)))
         
         motionManager = CMMotionManager()
         motionManager?.startAccelerometerUpdates()
@@ -117,10 +152,42 @@ class GameScene: SKScene {
         return (from.position.x - to.position.x) * (from.position.x - to.position.x) + (from.position.y - to.position.y) * (from.position.y - to.position.y)
     }
     
+    func resetGame() {
+        print("Reset game")
+        for child in self.children {
+           //Determine Details
+            guard let childBall = child as? Ball else { continue }
+            childBall.removeFromParent()
+        }
+        score = 0
+        
+        let ball = SKSpriteNode(imageNamed: "ballBlue")
+        let ballRadius = ball.frame.width / 2.0
+        
+        initializeBalls(ballRadius, self.view!, ball)
+    }
+    
+    func increaseScore(_ increment: Int) {
+        score += increment
+        
+        if score > highScore {
+            highScore = score
+            UserDefaults.standard.set(highScore, forKey: "high_score")
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         
         guard let position = touches.first?.location(in: self) else {return}
+        
+        let touchedNode = atPoint(position)
+        if touchedNode.name == "RestartButton" {
+            resetGame()
+            return
+        }
+        
         guard let tappedBall = nodes(at: position).first(where: {$0 is Ball}) as? Ball else {return}
         
         matchedBalls.removeAll(keepingCapacity: true)
@@ -128,7 +195,7 @@ class GameScene: SKScene {
         getMatchers(from: tappedBall)
         
         if matchedBalls.count >= 3 {
-            score += Int(pow(2, Double(min(matchedBalls.count, 16))))
+            increaseScore(Int(pow(2, Double(min(matchedBalls.count, 16)))))
             
             for ball in matchedBalls {
                 if let particles = SKEmitterNode(fileNamed: "Explosion") {
@@ -143,7 +210,9 @@ class GameScene: SKScene {
             }
         }
         
-        if matchedBalls.count >= 12 {
+        let omgCount = deviceIdiom == .phone ? 6 : 12
+        
+        if matchedBalls.count >= omgCount {
             let omg = SKSpriteNode(imageNamed: "omg")
             omg.position = CGPoint(x: frame.midX, y: frame.midY)
             omg.zPosition = 100
